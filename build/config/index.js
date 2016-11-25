90a5d3397244e0e270508a6bdb15fbb86faee4f9
wochap <gean.marroquin@gmail.com>
2016-11-25 16:03:27 -0500

    [feat] add public folder

--- build/config/index.js
+++ build/config/index.js
@@ -6,4 +6,5 @@ export const BROWSER_SYNC_PORT = 3000
 export const WEBPACK_SERVER_PORT = 8080
 export const projectRootPath = path.resolve(__dirname, '../../')
 export const projectSourcePath = path.resolve(projectRootPath, 'src')
+export const projectPublicPath = path.join(projectRootPath, 'public')
 export const templatePath = path.join(projectSourcePath, 'views/index.pug')
