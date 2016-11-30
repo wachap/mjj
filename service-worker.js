1bef1f7caf41681c00e215f0706c998bfc11665f
wochap <gean.marroquin@gmail.com>
2016-11-30 13:03:45 -0500

    Updates

--- service-worker.js
+++ service-worker.js
@@ -37,7 +37,7 @@
 /* eslint-disable indent, no-unused-vars, no-multiple-empty-lines, max-nested-callbacks, space-before-function-paren, quotes, comma-spacing */
 'use strict';
 
-var precacheConfig = [["/CNAME","ad6fd97af42d3343023402dc8f75448a"],["/app-manifest.json","d748cce8c86f3a601124e4fbafaf9360"],["/index.html","58ad423b1fec8fc8dfe99998ec782691"],["/static/css/app.32cac3aa.css","96c0bb4eea569f05a1db93467681a6f7"],["/static/js/app.933c7fc7.js","11f18141c3fc4c38086b510ab985998c"],["/static/js/manifest.1cdf0fd6.js","14e3add7bf23fb187786e4dea0883fea"],["/static/js/vendor.08a67257.js","0bd4d538ec92b680d51a84e3368f25f4"],["/webpack-assets.json","d45eb023e6289bd9bf194fc1f454677d"]];
+var precacheConfig = [["/CNAME","ad6fd97af42d3343023402dc8f75448a"],["/_config.yml","b9f40426128bae35fc61cb714d658e5c"],["/app-manifest.json","d748cce8c86f3a601124e4fbafaf9360"],["/index.html","ca37fa58ad5278f4b2014c48501d3582"],["/static/css/app.981104a5.css","09d0c4032a3f6ed7bbc1da37047aa1e2"],["/static/js/app.85bd5e7a.js","c472db1ebc8b9af26c6c5544ac41398a"],["/static/js/manifest.1b8b9826.js","4956319a5d9cc87687d908b477526b9f"],["/static/js/vendor.08a67257.js","0bd4d538ec92b680d51a84e3368f25f4"],["/webpack-assets.json","8a9f3d7048925dcebc3ee7d011a8ef4d"]];
 var cacheName = 'sw-precache-v2-m&j-' + (self.registration ? self.registration.scope : '');
 
 
@@ -249,7 +249,7 @@ self.addEventListener('fetch', function(event) {
 
 // Runtime cache configuration, using the sw-toolbox library.
 
-toolbox.router.get(/\.(svg, eot, ttf, woff, woff2, jpg, webp)$/, toolbox.networkFirst, {});
+toolbox.router.get(/\.(svg|eot|ttf|woff|woff2|jpg|webp|png)$/, toolbox.networkFirst, {});
 
 
 
